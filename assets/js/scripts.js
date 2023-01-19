811fe54d9f6257b5789883e145f7be581b9af147
Micael Santiago <santiagoo.micael04@gmail.com>
2023-01-19 17:54:38 -0300

    Atualização 2023.1

--- assets/js/scripts.js
+++
@@ -1,9 +0,0 @@
-window.onload = function(){
-	document.querySelector("#menu__mobile").addEventListener("click", function(){
-		if(document.querySelector(".header__nav nav ul").style.display == 'flex') {
-			document.querySelector(".header__nav nav ul").style.display = 'none';
-		} else {
-			document.querySelector(".header__nav nav ul").style.display = 'flex';
-		}
-	});
-};
---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+const nav = document.getElementById('nav');
+const menu = document.getElementById('menu');
+
+nav.onclick = () => {
+	menu.classList.toggle('mobile')
+}
